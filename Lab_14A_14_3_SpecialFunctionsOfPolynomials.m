b51b466a8b7ba48b77c0194151c8c3b67c6a50f9
Gustavo <glopezcelestino1@my.scccd.edu>
2026-05-14 02:44:20 +0000

    Last practice for the day

--- Lab_14A_14_3_SpecialFunctionsOfPolynomials.m
+++ Lab_14A_14_3_SpecialFunctionsOfPolynomials.m
@@ -1,1 +1,21 @@
+%Gustavo Lopez Celestino - Engr_5 - Lab 14.3 Special Functs. For
+%Polynomials
+
+%this script will ask the user for the coefficients and integration
+%constant of a cubic function (C). Will then report the functions for the
+%derivative and integral of the function
+
+coeffs = input(["Enter the coefficients for the cubic polynomial in " + ...
+    "the form [a b c d]\n"]);
+y0 = input("Enter the inegration constant for the function:\n");
+
+polyDer = polyder(coeffs);
+polyInt = polyint(coeffs,y0);
+
+fprintf("The derivative of the function is %gx^2 + %gx + %g\n",polyDer);
+fprintf("The integral of the function is %gx^4 + %gx^3 + %gx^2 + %gx + %g\n",polyInt);
+
+
+
+
 
